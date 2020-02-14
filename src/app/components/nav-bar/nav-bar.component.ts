ba45b5a74deabe81e614bf49ede3997d48f602d8
josevsousa <jose.vicente.de.sousa@gmail.com>
2020-02-14 19:25:27 -0300

    escondendo menu ao clicar em um item na navbar

--- src/app/components/nav-bar/nav-bar.component.ts
+++ src/app/components/nav-bar/nav-bar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-nav-bar',
@@ -7,9 +8,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavBarComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private router: Router
+  ) { }
 
   ngOnInit() {
+  }
+
+  hiddenMenuBar(){
+    console.log("ok");
+    var n = document.getElementById('navbarTogglerDemo03');
+    n.classList.remove("show");
+    this.router.navigate(['/contato']);
   }
 
 }
